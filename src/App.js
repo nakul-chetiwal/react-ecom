ee05a1379e64bb526025fc713d9b4098c31c7940
nakul-chetiwal <chetiwalnakul7@gmail.com>
2022-02-17 20:16:48 -0500

    set homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Homepage from "./Pages/Homepage";
 import "./default.scss";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+           <Header />
+      <Homepage />
     </div>
   );
 }
---
+++ src/Pages/Homepage/index.js
@@ -0,0 +1,11 @@
+import "./style.scss";
+
+const Homepage = (props) => {
+  return (
+    <section className="homepage">
+      <h1>Google Shop</h1>
+    </section>
+  );
+};
+
+export default Homepage;
