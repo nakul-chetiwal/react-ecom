f3ab8254823db5eff6517d7773c3fa4d784ea896
nakul-chetiwal <chetiwalnakul7@gmail.com>
2022-02-27 17:35:41 -0500

    Design cart page with data

--- src/Pages/Cart/index.js
+++ src/Pages/Cart/index.js
@@ -1,11 +1,43 @@
+import Button from "../../components/Button";
+import CartItem from "../../components/CartItem";
+import { connect } from "react-redux";
 import "./style.scss";
 
-const Cart = (props) => {
+const Cart = ({ cart }) => {
   return (
-    <section className="cartpage">
-      <h1>Cart page</h1>
+    <section>
+      <div className="container py-5">
+        <div className="row d-flex justify-content-center align-items-center">
+          <div className="col-lg-12">
+            <div className="d-flex justify-content-between align-items-center mb-4">
+              <div className="page-title">
+                <h1>CART</h1>
+              </div>
+            </div>
+            {cart.length ? (
+              cart.map((cartItem) => {
+                return <CartItem key={cartItem.id} cartItem={cartItem} />;
+              })
+            ) : (
+              <div>NO ITEMS IN THE CART</div>
+            )}
+            <div className="row mt-5">
+              <div className="col-lg-8 col-md-6 col-sm-12 col-xs-12"></div>
+              <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                <Button>Checkout</Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
 
-export default Cart;
+const mapStateToProps = (state) => {
+  return {
+    cart: state.shop.cart,
+  };
+};
+
+export default connect(mapStateToProps)(Cart);
---
+++ src/components/CartItem/index.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Button from "../../components/Button";
+import "./style.scss";
+
+function CartItem(props) {
+  const [qty, setQty] = useState(props.cartItem.qty);
+  const incrementQty = () => {
+    setQty(qty + 1);
+  };
+  const decrementQty = () => {
+    setQty(qty - 1);
+  };
+  return (
+    <div className="row d-flex justify-content-between align-items-center cart-item pt-5 pb-5">
+      <div className="col-lg-9">
+        <span className="text-uppercase d-block mt-5">
+          <h2>{props.cartItem.name}</h2>
+        </span>
+        <span className="text-uppercase text-muted d-block mt-2">
+          <h3>{props.cartItem.name}</h3>
+        </span>
+        <span className="text-uppercase d-block mt-2">
+          <h3>
+            {props.cartItem.prices[0].currency.symbol}
+            {props.cartItem.prices[0].amount}
+          </h3>
+        </span>
+      </div>
+      <div className="col-lg-1">
+        <Button onClick={incrementQty}>
+          <i className="fas fa-plus"></i>
+        </Button>
+        <div className="qty-input d-block">{qty}</div>
+        <Button className="btn" onClick={decrementQty}>
+          <i className="fas fa-minus"></i>
+        </Button>
+      </div>
+      <div className="col-lg-2">
+        <img
+          src={props.cartItem.gallery[0]}
+          className="img-fluid rounded-3"
+          alt={props.cartItem.name}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default CartItem;
