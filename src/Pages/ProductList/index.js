7c8259bcd808a39fb1c17799dd532854260a2737
nakul-chetiwal <chetiwalnakul7@gmail.com>
2022-03-07 09:52:54 -0500

    Added Dynamic Category Name in PLP

--- src/Pages/ProductList/index.js
+++ src/Pages/ProductList/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
 import { LOAD_ALL_PRODUCTS } from "../../GraphQL/Queries";
 import ProductListCard from "../../components/ProductListCard";
 import "./style.scss";
@@ -8,6 +9,7 @@ import { connect } from "react-redux";
 import { setAllProducts } from "../../redux/Shopping/shopping-actions";
 
 const ProductList = ({ setAllProducts }) => {
+  const { catID } = useParams();
   const { loading, error, data } = useQuery(LOAD_ALL_PRODUCTS, {
     variables: {
       input: { title: "clothes" },
@@ -26,8 +28,8 @@ const ProductList = ({ setAllProducts }) => {
     <section className="section-products">
       <div className="wrapper">
         <div className="container">
-          <div className="page-title">
-            <h1>MEN</h1>
+          <div className="page-title text-uppercase">
+            <h1>{catID}</h1>
           </div>
           <div className="row">
             {products.map((product, index) => {
